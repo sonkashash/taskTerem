57f01fa9ae8e700db82b6b93bce9142a1e532fca
Sofia <sophia.shashova@yandex.ru>
2024-02-21 16:06:30 +0300

    Task added

---
+++ script.js
@@ -0,0 +1,54 @@
+/* -----------------------FOR index.html----------------------- */
+$(document).ready(function () {
+    $(".btn-warning").click(function () {
+      console.log('ki')
+      $("#block0 .border").toggle();
+      $("#block0").toggle();
+    });
+    let blockMoving = false;
+    $(".btn-success").click(function () {
+      const $block1 = $("#block1");
+      const $block2 = $("#block2");
+      if (!blockMoving) {
+        $block1.insertAfter($block2);
+        blockMoving = true;
+        console.log('true')
+      } else {
+        $block1.insertBefore($block2);
+        blockMoving = false;
+        console.log('false')
+      }
+    });
+
+
+    $('#exampleModal').modal('show');
+  });
+/* -----------------------FOR forma.html----------------------- */
+$(document).ready(function () {
+    $("#Form").submit(function (event) {
+
+        event.preventDefault(); 
+        let formData = new FormData(this);
+        var jsonObject = {};
+        formData.forEach(function (value, key) {
+            jsonObject[key] = value;
+        });
+
+
+        $.ajax({
+            url: "handler.php",
+            type: "GET",
+            data: jsonObject,
+            success: function (response) {
+                alert("Данные успешно дошли");
+            },
+            error: function (xhr, status, error) {
+                console.error(status, error);
+            }
+        });
+
+        $("#json-output").text(JSON.stringify(jsonObject, null, 2));
+
+        $('#exampleModal').modal('show');
+    });
+});
