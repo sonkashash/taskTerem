c4a709c925901c1a8c125d48a8bcb3edcbc21ab0
Sofia <sophia.shashova@yandex.ru>
2024-03-03 19:18:00 +0300

    Delete important

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
 /* -----------------------FOR index.html----------------------- */
 $(document).ready(function () {
-    $(".btn-warning").click(function () {
-      $("#block0").toggle();
-    });
-    let blockMoving = false;
-    $(".btn-success").click(function () {
-      const $block1 = $("#block1");
-      const $block2 = $("#block2");
-      if (!blockMoving) {
-        $block1.insertAfter($block2);
-        blockMoving = true;
-      } else {
-        $block1.insertBefore($block2);
-        blockMoving = false;
-      }
-    });
-
-
-    $('#exampleModal').modal('show');
+  $(".btn-warning").click(function () {
+    $("#block0").toggle();
   });
+  let blockMoving = false;
+  $(".btn-success").click(function () {
+    const $block1 = $("#block1");
+    const $block2 = $("#block2");
+    if (!blockMoving) {
+      $block1.insertAfter($block2);
+      blockMoving = true;
+    } else {
+      $block1.insertBefore($block2);
+      blockMoving = false;
+    }
+  });
+
+});
 /* -----------------------FOR forma.html----------------------- */
 $(document).ready(function () {
-    $("#Form").submit(function (event) {
-
-        event.preventDefault(); 
-        let formData = new FormData(this);
-        var jsonObject = {};
-        formData.forEach(function (value, key) {
-            jsonObject[key] = value;
-        });
-
-
-        $.ajax({
-            url: "handler.php",
-            type: "GET",
-            data: jsonObject,
-            success: function (response) {
-                alert("Данные успешно дошли");
-            },
-            error: function (xhr, status, error) {
-                console.error(status, error);
-            }
-        });
-
-        $("#json-output").text(JSON.stringify(jsonObject, null, 2));
-
-        $('#exampleModal').modal('show');
+  $("#Form").submit(function (event) {
+    event.preventDefault();
+    let formData = new FormData(this);
+    var jsonObject = {};
+    formData.forEach(function (value, key) {
+      jsonObject[key] = value;
     });
+
+    $.ajax({
+      url: "handler.php",
+      type: "GET",
+      data: jsonObject,
+      success: function (response) {
+        alert("Данные успешно дошли");
+      },
+      error: function (xhr, status, error) {
+        console.error(status, error);
+      },
+    });
+
+    $("#json-output").text(JSON.stringify(jsonObject, null, 2));
+
+  });
 });
