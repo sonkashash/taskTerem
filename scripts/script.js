21b1ec56a037430a345c69da47d86bd793bc76e0
Sofia <sophia.shashova@yandex.ru>
2024-03-06 15:40:12 +0300

    Task forma changed

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
-/* -----------------------FOR index.html----------------------- */
 const header = document.querySelector(".header");
 const yellowBlock = document.querySelector(".yellow");
 const greenBlock = document.querySelector(".green");
+const form = document.querySelector(".form");
+const output = document.querySelector(".output");
+const formData = {};
+const url = "../handler.php";
+
+/* -----------------------FOR index.html----------------------- */
 
 const blockDelete = (el) => {
   el.classList.toggle("blockVisibility");
@@ -15,6 +20,44 @@ const blockChange = (el1, el2) => {
 };
 
 /* -----------------------FOR forma.html----------------------- */
+
+const formSubmit = async () => {
+  form.querySelectorAll("select, input").forEach((el) => {
+    formData[el.name] = el.value;
+  });
+  output.innerText = JSON.stringify(formData, null, 2);
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch");
+    }
+
+    const responseData = await response.text();
+    alert("Response from server: " + responseData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// async function submitForm() {
+//   const formData = new FormData(document.querySelector(".form"));
+//   const jsonData = JSON.stringify(Object.fromEntries(formData));
+
+//   // const url = `handler.php?dropdown1=${dropdown1}&dropdown2=${dropdown2}&dropdown3=${dropdown3}&dropdown4=${dropdown4}&dropdown5=${dropdown5}&input1=${input1}&input2=${input2}`;
+
+//   try {
+//     const response = await fetch('../handler.php', {
+//       method: "GET",
+//     });
+//     if (!response.ok) {
+//       throw new Error("Ошибка HTTP: " + response.status);
+//     }
+//     alert("Данные дошли успешно");
+//   } catch (error) {
+//     console.error("Ошибка:", error);
+//   }
 // $(document).ready(function () {
 //   $("#Form").submit(function (event) {
 //     event.preventDefault();
